5833fa758c7989266665cb45d67a2a4b5aea8e08
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2024-06-03 14:35:44 +0900

    feat: add jwt auth guard

---
+++ src/user/strategy/jwt.strategy.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req) => {
+          let jwt = null;
+          if (req && req.cookies) {
+            jwt = req.cookies['access_token'];
+          }
+          return jwt;
+        },
+      ]),
+      ignoreExpiration: false, // falseにしないとjwtの有効期限が切れても有効になってしまう場合がある
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  // nestJsのvalidateは返り値（ここでいうreturn user）を自動にRequestに含めてくれる機能がある
+  // そうなるとControllerの方でRequestでuserを取り出すことが可能になる
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+    delete user.hashedPassword;
+    return user;
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client';
+import { UserService } from './user.service';
 
+// UseGuards(AuthGuard)で認証関係のプロテクトをしている 今回はjwtを指定してガードしている
+// ただし、jwtがHeaderに含まれたり、今回のようにcookieに含まれたりと様々な場合があるのでjwtStrategyファイルを作成してカスタマイズする
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    return req.user;
+  }
+
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
+  }
+}
