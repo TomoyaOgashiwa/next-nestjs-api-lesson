c813619874b18f5196b8a6532614871ff7d66cd0
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2024-06-03 15:49:47 +0900

    feat: add csrf token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -17,6 +17,12 @@ import { Csrf, Msg } from './interfaces/auth.interface';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // httpOnly: trueにすることで自動的にcookieに付与される形
+  @Get('csrf')
+  getCsrfToken(@Req() req: Request): Csrf {
+    return { csrfToken: req.csrfToken() };
+  }
+
   @Post('signup')
   signUp(@Body() dto: AuthDto): Promise<Msg> {
     return this.authService.signUp(dto);
@@ -34,7 +40,7 @@ export class AuthController {
     const jwt = await this.authService.login(dto);
     res.cookie('access_token', jwt.accessToken, {
       httpOnly: true,
-      secure: false, // ここをtrueにするとhttpsでの通信が必要になるのでローカル確認する場合はfalseにしておく
+      secure: true, // ここをtrueにするとhttpsでの通信が必要になるのでローカル確認する場合はfalseにしておく
       sameSite: 'none',
       path: '/',
     });
@@ -48,7 +54,7 @@ export class AuthController {
   logout(@Req() req: Request, @Res({ passthrough: true }) res: Response): Msg {
     res.cookie('access_token', '', {
       httpOnly: true,
-      secure: false, // ここをtrueにするとhttpsでの通信が必要になるのでローカル確認する場合はfalseにしておく
+      secure: true, // ここをtrueにするとhttpsでの通信が必要になるのでローカル確認する場合はfalseにしておく
       sameSite: 'none',
       path: '/',
     });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// import { Request } from 'express';
+import { Request } from 'express';
 import * as cookieParser from 'cookie-parser';
-// import * as csurf from 'csurf';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +19,18 @@ async function bootstrap() {
   });
 
   app.use(cookieParser());
-  await app.listen(3005);
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true, // Javascriptから読み込まれたくない場合にtrueにする
+        sameSite: 'none',
+        secure: true,
+      },
+      value: (req: Request) => {
+        return req.header('csrf-token');
+      },
+    }),
+  );
+  await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
